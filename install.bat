2c23da112b34afdbb4fd4ee8d7fd13356ee25995
anton7r <you@example.com>
2019-08-02 23:44:59 +0300

    managing codebase

--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title a7JS Installer
-echo --------------------------
-echo  Starting to install a7JS
-echo --------------------------
-
-robocopy .\src "%programfiles%"\a7js /MIR
-
-echo a7JS was copied.
