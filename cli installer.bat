389e0faeeddf6b9d46d7a8e3d2c4a807d7be1264
anton7r <you@example.com>
2019-08-03 19:34:16 +0300

    more renaming

--- cli installer.bat
+++ cli installer.bat
@@ -1,6 +1,6 @@
 @echo off
 
 echo installing a7JS..
-xcopy /y %cd%\scripts\a7.bat  %windir%\System32\a7.bat /C/Q
+xcopy /y %cd%\cli\a7.bat  %windir%\System32\a7.bat /C/Q
 xcopy %cd%\src "%ProgramFiles%\a7js\" /E/C/Y/Q
 echo a7JS installation complete.
