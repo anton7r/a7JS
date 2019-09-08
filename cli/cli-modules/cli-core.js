b21ce9a9c46686fad1d449afd2d66fcb725baf2c
anton7r <you@example.com>
2019-09-08 22:51:43 +0300

    importer moved to another file

--- cli/cli-modules/cli-core.js
+++ cli/cli-modules/cli-core.js
@@ -5,21 +5,7 @@ const chalk = require("chalk");
 const clicore = {};
 const log = console.log;
 
-clicore.self = "a7js";
-
-
 clicore.pathToA7JS = require.resolve("../../src/a7.js");
-
-
-//Since the cli is inside the a7JS node cannot find the module a7js for some reason.
-clicore.replaceSelf = function(Module){
-    if(Module === clicore.self){
-        return clicore.pathToA7JS;
-    } else {
-        return Module;
-    }
-};
-
 
 clicore.componentSource = function (string){
     return string.match(/\".+\"/g)[0].replace(/\"/g, "");
@@ -63,57 +49,6 @@ clicore.syntaxLog = function(syntax){
     log(chalk.gray(" - Syntax: " + syntax));
 };
 
-clicore.importFrom = function(ImportStatement){
-    return ImportStatement.match(/\".+\"/i)[0].replace(/\"/g, "");
-};
-
-clicore.importName = function(ImportStatement){
-    return ImportStatement.replace(/import\s*/, "").replace(/\s*from\s*\".+?\";*/, "");
-};
-
-clicore.importer = function(sourceCode){
-    var moduleExport = /module.exports\s*=\s*(\w|\d)*;*/g;
-    var wholeImports = sourceCode.match(/import\s+(\d|\w)+\s+from\s*\".+\";*/gi);
-    var partialImports = sourceCode.match(/import\s*{\s*.*?\s*}\s+from\s*\".+?\";*/gi);
-    //TODO:(Pro tip) make foreach into for loop because it is faster
-    this.imports = 0;
-    
-    if (wholeImports !== null){
-        this.imports += wholeImports.length;
-        wholeImports.forEach(Import => {
-            var importFrom = clicore.replaceSelf(clicore.importFrom(Import));
-            var importName = clicore.importName(Import);
-            var importableModule = require.resolve(importFrom);
-            var moduleSourceCode = fs.readFileSync(importableModule, "utf-8");
-
-            var exportDefaultName = "";
-            var moduleSourceCodeMatches = moduleSourceCode.match(moduleExport);
-            if(moduleSourceCodeMatches !== null){
-                console.log(moduleSourceCodeMatches);
-                moduleSourceCode = moduleSourceCode.replace(moduleSourceCodeMatches[0], "");
-                exportDefaultName = moduleSourceCodeMatches[0].replace(/module.exports\s*=\s*/, "").replace(/;/, "");
-
-            }
-            var importedModule = `/* `+ Import +` */(function(window){` + moduleSourceCode + ` if(typeof (window.` + importName + `) === "undefined"){window.` + importName + `=` + exportDefaultName + `}})(window)`;
-            sourceCode = sourceCode.replace(Import, importedModule);
-        });
-    
-    }
-
-    if (partialImports !== null){
-        this.imports += partialImports.length;
-        partialImports.forEach(Import =>{
-            var importFrom = clicore.importFrom(Import);
-        });
-        return clicore.errorLog("Importing only a part of a framework is not yet supported!");
-    }
-
-    //Development helpers
-    log("whole imports:"+wholeImports);
-    log("partial imports:"+partialImports);
-    this.output = sourceCode;
-    return this;
-};
-
+clicore.importer = require("./cli-importer.js");
 
 module.exports = clicore;
---
+++ cli/cli-modules/cli-importer.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const log = console.log;
+const pathToA7JS = require.resolve("../../src/a7.js");
+const self = "a7js";
+//Since the cli is inside the a7JS node cannot find the module a7js for some reason.
+const replaceSelf = function(Module){
+    if(Module === self){
+        return pathToA7JS;
+    } else {
+        return Module;
+    }
+};
+
+const importFrom = function(ImportStatement){
+    return ImportStatement.match(/\".+\"/i)[0].replace(/\"/g, "");
+};
+
+const importName = function(ImportStatement){
+    return ImportStatement.replace(/import\s*/, "").replace(/\s*from\s*\".+?\";*/, "");
+};
+
+module.exports = function(sourceCode){
+    var imports = [];
+    var moduleExport = /module.exports\s*=\s*(\w|\d)*;*/g;
+    var wholeImports = sourceCode.match(/import\s+(\d|\w)+\s+from\s*\".+\";*/gi);
+    var partialImports = sourceCode.match(/import\s*{\s*.*?\s*}\s+from\s*\".+?\";*/gi);
+    //TODO:(Pro tip) make foreach into for loop because it is faster
+    this.imports = 0;
+    
+    if (wholeImports !== null){
+        this.imports += wholeImports.length;
+        wholeImports.forEach(Import => {
+            var importNameVar = importName(Import);
+            var importableModule = require.resolve(replaceSelf(importFrom(Import)));
+            var moduleSourceCode = fs.readFileSync(importableModule, "utf-8");
+
+            var exportDefaultName = "";
+            var moduleSourceCodeMatches = moduleSourceCode.match(moduleExport);
+            if(moduleSourceCodeMatches !== null){
+                console.log(moduleSourceCodeMatches);
+                moduleSourceCode = moduleSourceCode.replace(moduleSourceCodeMatches[0], "");
+                exportDefaultName = moduleSourceCodeMatches[0].replace(/module.exports\s*=\s*/, "").replace(/;/, "");
+
+            }
+            var importedModule = `/* `+ Import +` */(function(window){` + moduleSourceCode + ` if(typeof (window.` + importNameVar + `) === "undefined"){window.` + importNameVar + `=` + exportDefaultName + `}})(window)`;
+            sourceCode = sourceCode.replace(Import, importedModule);
+            imports += {from:importableModule,as:importNameVar};
+        });
+    
+    }
+
+    if (partialImports !== null){
+        this.imports += partialImports.length;
+        partialImports.forEach(Import =>{
+            var importFromVar = importFrom(Import);
+        });
+        return clicore.errorLog("Importing only a part of a framework is not yet supported!");
+    }
+    log(imports);
+    //Development helpers
+    log("whole imports:"+wholeImports);
+    log("partial imports:"+partialImports);
+    log("imported", this.imports, module);
+    this.output = sourceCode;
+    return this;
+};
