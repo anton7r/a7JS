b6d5d809d4f044768b0b8149d24b4d4d71216c4c
anton7r <you@example.com>
2019-09-09 23:02:08 +0300

    component import detection

--- cli/cli-modules/cli-importer.js
+++ cli/cli-modules/cli-importer.js
@@ -3,6 +3,7 @@ const log = console.log;
 const pathToA7JS = require.resolve("../../src/a7.js");
 const self = "a7js";
 const uglifyJS = require("uglify-js");
+
 //Since the cli is inside the a7JS node cannot find the module a7js for some reason.
 const replaceSelf = function(Module){
     if(Module === self){
@@ -13,21 +14,46 @@ const replaceSelf = function(Module){
 };
 
 const importFrom = function(ImportStatement){
-    return ImportStatement.match(/\".+\"/i)[0].replace(/\"/g, "");
+    return ImportStatement.match(/(\"|\').+(\"|\')/i)[0].replace(/\"/g, "");
 };
 
 const importName = function(ImportStatement){
     return ImportStatement.replace(/import\s*/, "").replace(/\s*from\s*\".+?\";*/, "");
 };
 
+const eliminateComponents = function(val){
+    if(val.match(/"\.\/components\/.+?\"/) !== null){
+        return false;
+    } else {
+        return true;
+    }
+};
+
 module.exports = function(sourceCode){
     var imports = [];
     var moduleExport = /module.exports\s*=\s*(\w|\d)*;*/g;
-    var wholeImports = sourceCode.match(/import\s+(\d|\w)+\s+from\s*\".+\";*/gi);
+
+    var componentImports = sourceCode.match(/import\s+(\d|\w|\_)+\s+from\s*\"\.\/components\/.+?\";*/gi);
+    var wholeImports = sourceCode.match(/import\s+(\d|\w|\_)+\s+from\s*\".+\";*/gi);
+
+    //Whole imports eliminate component imports
+    wholeImports = wholeImports.filter(eliminateComponents);
+
     var partialImports = sourceCode.match(/import\s*{\s*.*?\s*}\s+from\s*\".+?\";*/gi);
     //TODO:(Pro tip) make foreach into for loop because it is faster
     this.imports = 0;
     
+    if (componentImports !== null){
+        this.imports += componentImports.length;
+        componentImports.forEach(Import => {
+            var importNameVar = importName(Import);
+            var importableModule = importFrom(Import);
+
+            log(importableModule);
+            var componentSourceCode = fs.readFileSync("./app/" + importableModule.replace(/(\.|\.\/)/, ""), "utf-8");
+        });
+    }
+
     if (wholeImports !== null){
         this.imports += wholeImports.length;
         wholeImports.forEach(Import => {
@@ -38,9 +64,8 @@ module.exports = function(sourceCode){
             var exportDefaultName = "";
             var moduleSourceCodeMatches = moduleSourceCode.match(moduleExport);
             if(moduleSourceCodeMatches !== null){
-                console.log(moduleSourceCodeMatches);
                 moduleSourceCode = moduleSourceCode.replace(moduleSourceCodeMatches[0], "");
-                exportDefaultName = moduleSourceCodeMatches[0].replace(/module.exports\s*=\s*/, "").replace(/;/, "");
+                exportDefaultName = moduleSourceCodeMatches[0].replace(/(module.exports\s*=\s*|;)/g, "");
 
             }
             var importedModule = `(function(window){` + moduleSourceCode + ` if(typeof (window.` + importNameVar + `) === "undefined"){window.` + importNameVar + `=` + exportDefaultName + `}})(window)`;
