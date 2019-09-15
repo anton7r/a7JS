8f00adfe1b3733e2c215c093de01f6125181ae2a
anton7r <you@example.com>
2019-09-15 23:03:14 +0300

    started to make building work

--- cli/cli-modules/cli-htmlcompiler.js
+++ cli/cli-modules/cli-htmlcompiler.js
@@ -12,9 +12,9 @@ var htmlCopy3 = html;
 
 var compiledStringifiedArray = "";
 
-var headElements;
+var headElements = [];
 
-var ElementStarting = html.match(/\<(\d|\w)+?\s*([^>]*?)[^\/]>/g);
+var ElementStarting = html.match(/\<[^<>/\s]+\s*[^<>]*?(?<=[^/])\>/g);
 
 var ElementClosing = html.match(/\<\/(\w|\d)+?\>/g);
 
@@ -28,6 +28,11 @@ indexOF.ElementStarting = [];
 indexOF.ElementClosing = [];
 indexOF.ElementSelfClosing = [];
 
+var lastIndexOF = {};
+lastIndexOF.ElementStarting = [];
+lastIndexOF.ElementClosing = [];
+lastIndexOF.ElementSelfClosing = [];
+
 var tagOF = {};
 tagOF.ElementStarting = [];
 tagOF.ElementClosing = [];
@@ -38,9 +43,11 @@ var Jump = 0;
 if(ElementStarting !== null&& ElementStarting !== undefined){
     ElementStarting.forEach(val => {
         var index = htmlCopy1.indexOf(val) + Jump;
+        var lIndex = htmlCopy1.lastIndexOf(val) + Jump;
         htmlCopy1 = htmlCopy1.replace(val, "");
         Jump += val.length;
         indexOF.ElementStarting.push(index);
+        lastIndexOF.ElementStarting.push(lIndex);
         tagOF.ElementStarting.push(getElTag(val));
     });
 }
@@ -49,11 +56,13 @@ Jump = 0;
 if(ElementClosing !== null&& ElementClosing !== undefined){
     ElementClosing.forEach(val => {
         var index = htmlCopy2.indexOf(val) + Jump;
+        var lIndex = htmlCopy2.lastIndexOf(val) + Jump;
         htmlCopy2 = htmlCopy2.replace(val, "");
 
         Jump += val.length;
         tagOF.ElementClosing.push(getElTag(val));
         indexOF.ElementClosing.push(index);
+        lastIndexOF.ElementClosing.push(lIndex);
     });
 }
 
@@ -61,32 +70,46 @@ Jump = 0;
 if(ElementSelfClosing !== null&& ElementSelfClosing !== undefined){
     ElementSelfClosing.forEach(val => {
         var index = htmlCopy3.indexOf(val) + Jump;
+        var lIndex = htmlCopy3.lastIndexOf(val) + Jump;
         htmlCopy3 = htmlCopy3.replace(val, "");
 
         Jump += val.length;
         indexOF.ElementSelfClosing.push(index);
         tagOF.ElementSelfClosing.push(getElTag(val));
+        lastIndexOF.ElementSelfClosing.push(lIndex);
     });
 }
 
 console.log(tagOF.ElementStarting);
 console.log(tagOF.ElementClosing);
-
+console.log(lastIndexOF.ElementStarting);
+console.log(lastIndexOF.ElementClosing);
 //console.log(ElementStarting);
 //console.log(ElementSelfClosing);
 //console.log(ElementClosing);
 
 
+//building algo
+//Get topLevel Elements first
+var loc = 0; 
+ElementStarting.forEach(function(val){
+    //findClosing
+    var nextStart = indexOF.ElementStarting[loc + 1];
+    var close = indexOF.ElementClosing[loc];
+    var content = "";
+    var el;
+    //safe close
+    if(nextStart > close | nextStart === undefined){
+        content = html.slice(indexOF.ElementStarting[loc] + val.length, indexOF.ElementClosing[loc]);
+        el = "a7.createElement(\'"+tagOF.ElementStarting[loc]+"\',{},\'"+content+"\')";    
+        headElements.push(el);
+    }
+    
+    
 
 
-
-
-
-
-
-
-
-
+    loc++;
+});
 
 if(headElements !== null&& headElements !== undefined){
     headElements.forEach(function(el){
@@ -96,6 +119,7 @@ if(headElements !== null&& headElements !== undefined){
 
 var compiled = "a7.documentFragment("+ compiledStringifiedArray + ")";
 
+console.log(compiled);
 return compiled;
 
 };
