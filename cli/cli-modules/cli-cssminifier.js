c7c7a34f6c18507c8188ca97770af4a4aeeae124
anton7r <you@example.com>
2019-09-22 18:47:50 +0300

    css minification does not crash the server

--- cli/cli-modules/cli-cssminifier.js
+++ cli/cli-modules/cli-cssminifier.js
@@ -1,5 +1,17 @@
+const clicore = require("./cli-core");
 const csso = require("csso");
 
-module.exports = function(css){
-    return csso.minify(css).css;
+module.exports = function minifier(css){
+
+    try {
+        var min = csso.minify(css);
+        return min.css;
+    
+    }
+    
+    catch(e){
+        clicore.errorLog("Could not minify css.");
+        return css.replace(/\n\r/g, "");
+    }
+
 };
