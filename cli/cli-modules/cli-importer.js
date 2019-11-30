23541c54cd18e893bf52029229589c4c8f39b12c
anton7r <52160345+anton7r@users.noreply.github.com>
2019-11-30 22:35:25 +0200

    refactoring

--- cli/cli-modules/cli-importer.js
+++ cli/cli-modules/cli-importer.js
@@ -2,7 +2,6 @@
 /* jshint -W119 */
 /* jshint -W049 */
 const fs = require("fs");
-const log = console.log;
 const uglifyJS = require("uglify-js");
 const core = require("./cli-core");
 const cssMinifier = require("./cli-cssminifier");
@@ -11,48 +10,48 @@ const csso = require("csso");
 var config = core.config;
 
 const minifier = function (source){
-    source = purePath(source);
     try {
-        var min = uglifyJS.minify(source);
-        if(min.code === undefined){
+        var m = uglifyJS.minify(source);
+        if(m.code === undefined){
             return source;
         }
-        return min.code;
+        return m.code;
     } catch (e){
         core.errorLog("an error happened while trying to minify a script");
         return source;
     }
 };
 
-const multiReplace = function(from){
-    const args = arguments;
-    for(let i = 1; i < arguments.length; i++){
-        let c = args[i];
-        from = from.replace(c[0], c[1]);
+//replace multiple things from a string;
+const multiReplace = function(s){
+    const a = arguments;
+    for(let i = 1; i < a.length; i++){
+        s = s.replace(a[i][0], a[i][1]);
     }
-    return from;
+    return s;
 }
 
 const existsRead = function (path){
+    path = purePath(path);
     if(fs.existsSync(path)){
         return fs.readFileSync(path, "utf-8");
     } else {
         core.errorLog("file could not be located. "+ path);
-        return process.exit(); 
+        process.exit(); 
     }
 };
 
-const componentSource = function (string){
-    return string.match(/\".+\"/g)[0].replace(/\"/g, "");
+const componentSource = function (str){
+    return str.match(/\".+\"/g)[0].replace(/\"/g, "");
 };
 
-const getEntryFolder = function(){
-    if(config.entry !== "noEntry"){
-        return config.entry.replace(/(\w|\d)+\.js/i, "");
-    }
-};
-
-const entryFolder = getEntryFolder();
+let entryFolder = "";
+if(config.entry !== "noEntry"){
+    entryFolder = config.entry.replace(/(\w|\d)+\.js/i, "");
+} else {
+    core.errorLog("no entry to your application was defined in a7.config.json");
+    process.exit();
+}
 
 //purePath is our innovative technology to "relational pairing" from file paths to make them work on any given file system.
 //for example if you have ../ or ./ in the path purePath will happily remove it.
@@ -66,23 +65,15 @@ const importHandler = function(imp){
     return this;
 };
 
-const eliminateComponents = function(val){
-    if(val.match(/"\.\/components\/.+?\"/) !== null){
-        return false;
-    } else {
-        return true;
-    }
-};
 //Searches properties from objects in sourceCode
-function findProp (findFrom, find){
-    return findFrom.match(new RegExp(find + "\s*:\s*\".+?\"", "i"))[0];
+function findProp (from, find){
+    return from.match(new RegExp(find + "\s*:\s*\".+?\"", "i"))[0];
 }
 
-//splits css into non component container affecting to component container affecting ones
-const cssSplitter = function(csssrc, componentTag){
+const cssSplitter = function(csssrc, tag){
     //container RegExp
-    containerRx = new RegExp(componentTag, "g");
-    containerStylesRx = new RegExp(componentTag + "(.|\s)+?\{(.|\s)+?\}", "g");
+    containerRx = new RegExp(tag, "g");
+    containerStylesRx = new RegExp(tag + "(.|\s)+?\{(.|\s)+?\}", "g");
     var containerStyles = csssrc.match(containerStylesRx);
     var parsedContainerStyles = "";
 
@@ -91,7 +82,7 @@ const cssSplitter = function(csssrc, componentTag){
             parsedContainerStyles += style;
         });
     }
-    parsedContainerStyles = parsedContainerStyles.replace(containerRx, ".a7-component." + componentTag);
+    parsedContainerStyles = parsedContainerStyles.replace(containerRx, ".a7-component." + tag);
     return {container:parsedContainerStyles,innerStyles:csssrc.replace(containerStylesRx, "")};     
 };
 
@@ -109,137 +100,142 @@ module.exports = function(sourceCode){
     }
 
     var imports = [];
-    var moduleExportEquals = /module.exports\s*=\s*(\w|\d)*;*/g;
-
     var componentImports = sourceCode.match(/import\s+(\d|\w|\_)+\s+from\s*\"\.\/components\/.+?\";*/gi);
     var wholeImports = sourceCode.match(/import\s+(\d|\w|\_)+\s+from\s*\".+\";*/gi);
 
     //Whole imports eliminate component imports
-    wholeImports = wholeImports.filter(eliminateComponents);
+    wholeImports = wholeImports.filter(function(val){
+        if(val.match(/"\.\/components\/.+?\"/) !== null) return false;
+        else return true;
+    });
 
     var partialImports = sourceCode.match(/import\s*{\s*.*?\s*}\s+from\s*\".+?\";*/gi);
     //TODO:(Pro tip) make foreach into for loop because it is faster
     this.imports = 0;
     
+    //Goes through component imports
+    var len = 0;
     if (componentImports !== null){
         this.imports += componentImports.length;
-        componentImports.forEach(Import => {
-            //imp means the imported object
-            var imp = importHandler(Import);
-            //path to component folder
-            var folder = purePath(entryFolder +imp.path.replace(/(\w|\n)+\.js/g, ""));
-            
-            //Component sourcecode
-            var componentSrc = multiReplace(
-                existsRead(entryFolder + imp.path),
-                [/export default function\s*\(/, "function e("],
-                [/export default function/, "function"]
-            );
-
-            var componentSetup = componentSrc.match(/return\s*\(\{(.|\s)*\}\)/)[0];
-            var htmlPath = componentSource(findProp(componentSetup, "template"));
-            var CSSPath = componentSource(findProp(componentSetup, "styles"));
-            var tag = findProp(componentSetup, "tag");
-            tag = tag.match(/\".+?\"/)[0].replace(/\"/g, "");
-            
-            htmlPath = purePath(folder + htmlPath);
-            CSSPath = purePath(folder + CSSPath);
-
-            var css = existsRead(CSSPath).replace(/\s+/g, " ");
-            var html = "a7.documentFragment(" + htmlCompiler(existsRead(htmlPath)) + ")";
-            //replace literals
-            templateLiterals = html.match(/{{\s*.+?\s*}}/);
-            if(templateLiterals !== null){
-                templateLiterals.forEach(function(literal){
-                    var cleanLiteral = literal.replace(/({{|}})/g, "");
-                    html = html.replace(literal, "\'+"+cleanLiteral+"+\'");
-                });
-            }
-            var cssObject = cssSplitter(css, tag);
-            var cssRules = cssObject.innerStyles.match(/.+?\s*?\{.+?\}/g);
-            if(cssRules !== null){
-                cssRules.forEach(function (rule){
-                    CSSBundle += ".a7-component." + tag+ " " + rule;
-                });
-            }
-
-            if (cssObject.container != ""){
-                CSSBundle += cssObject.container;
-            }
-
-            var out = minifier(multiReplace(componentSrc,
-                [componentSetup, "return " + html],
-                [/((\'\')\s*\+\s*|(\s*\+\s*\'\'))/g, ""]
-            ));
-
-            var exec = "a7.registerComponent(\""+tag+"\"," + out + ");function "+imp.name+"(a){return a7.createElement(\""+tag+"\",a)}";
-            sourceCode = sourceCode.replace(Import, exec);
-            imports += {from:imp.path,as:imp.name};
-        });
+        len = componentImports.length;
     }
+    for(let i = 0; i < len; i++){
+        var Import = componentImports[i];
+        //imp means the imported object
+        var imp = importHandler(Import);
+        //path to component folder
+        var folder = purePath(entryFolder +imp.path.replace(/(\w|\n)+\.js/g, ""));
+        
+        //Component sourcecode
+        var componentSrc = multiReplace(
+            existsRead(entryFolder + imp.path),
+            [/export default function\s*\(/, "function e("],
+            [/export default function/, "function"]
+        );
 
+        var componentSetup = componentSrc.match(/return\s*\(\{(.|\s)*\}\)/)[0];
+        var htmlPath = folder + componentSource(findProp(componentSetup, "template"));
+        var CSSPath = folder + componentSource(findProp(componentSetup, "styles"));
+        var tag = findProp(componentSetup, "tag");
+        tag = tag.match(/\".+?\"/)[0].replace(/\"/g, "");
+
+        var css = existsRead(CSSPath).replace(/\s+/g, " ");
+        var html = "a7.documentFragment(" + htmlCompiler(existsRead(htmlPath)) + ")";
+        //replace literals
+        templateLiterals = html.match(/{{\s*.+?\s*}}/);
+        if(templateLiterals !== null){
+            templateLiterals.forEach(function(literal){
+                var clean = literal.replace(/({{|}})/g, "");
+                html = html.replace(literal, "\'+"+clean+"+\'");
+            });
+        }
+        var cssObject = cssSplitter(css, tag);
+        var cssRules = cssObject.innerStyles.match(/.+?\s*?\{.+?\}/g);
+        if(cssRules !== null){
+            cssRules.forEach(function (rule){
+                CSSBundle += ".a7-component."+tag+" "+rule;
+            });
+        }
+
+        if (cssObject.container != ""){
+            CSSBundle += cssObject.container;
+        }
+
+        var out = minifier(multiReplace(componentSrc,
+            [componentSetup, "return " + html],
+            [/((\'\')\s*\+\s*|(\s*\+\s*\'\'))/g, ""]
+        ));
+
+        var exec = "a7.registerComponent(\""+tag+"\","+out+");function "+imp.name+"(a){return a7.createElement(\""+tag+"\",a)}";
+        sourceCode = sourceCode.replace(Import, exec);
+        imports += {from:imp.path,as:imp.name};
+    };
+
+    len = 0;
     if (wholeImports !== null){
         this.imports += wholeImports.length;
-        wholeImports.forEach(Import => {
-            var imp = importHandler(Import);
-            
-            //if the package is a7js, it will go searching for it 
-            if(imp.path === "a7js"){
-                imp.path = require.resolve("../../src/a7.js");
-            } else if(imp.path.charAt(0) === "."){
-                imp.path = purePath("./app/" + imp.path)
-            } else {
-                imp.path = require.resolve(imp.path);
-            }
-            //modSrc is moduleSourceCode
-            var modSrc = fs.readFileSync(imp.path, "utf-8");
-            //modImp finds modules imports
-            var modImp = modSrc.match(/(import\s+.+?\s+from\".*?\"|require\(.*?\))/g);
-
-            if(modImp !== null){
-                core.errorLog("Module " + imp.name +" has its own imports which we cannot right now import with our detections!");
-                return;
-            } else if(config.mode === "production"){
-                modSrc = minifier(modSrc);
-            }
-
-            var exportName = "";
-            var modExp = modSrc.match(moduleExportEquals);
-            if(modExp !== null){
-                modSrc = modSrc.replace(modExp[0],"");
-                exportName = modExp[0].replace(/(module.exports\s*=\s*|;)/g, "");
-            }
-
-            var mod = `;(function(){${modSrc} a7_i.${imp.name}=${exportName};})();var ${imp.name}=a7_i.${imp.name};`;
-
-            if(config.mode === "production"){
-                mod = minifier(mod);
-            }
-            //replacing the import on the sourcecode with the modules contens
-            sourceCode = sourceCode.replace(Import, "/* " + imp.name + " */" + mod);
-            imports += {
-                from:imp.path,
-                as:imp.name
-            };
-        });
+        len = wholeImports.length;
     }
+    for (let i = 0; i < len; i++){
+        var Import = wholeImports[i];
+        var imp = importHandler(Import);
+        
+        //if the package is a7js, it will go searching for it 
+        if(imp.path === "a7js"){
+            imp.path = require.resolve("../../src/a7.js");
+        } else if(imp.path.charAt(0) === "."){
+            imp.path = purePath("./app/" + imp.path)
+        } else {
+            imp.path = require.resolve(imp.path);
+        }
+        var modSrc = fs.readFileSync(imp.path, "utf-8");
+        //modImp finds modules imports
+        var modImp = modSrc.match(/(import\s+.+?\s+from\".*?\"|require\(.*?\))/g);
+        if(modImp !== null){
+            core.errorLog("Module " + imp.name +" has its own imports which we cannot right now import with our detections!");
+            return;
+        } else if(config.mode === "production"){
+            modSrc = minifier(modSrc);
+        }
 
+        var expName = "";
+        var modExp = modSrc.match(/module.exports\s*=\s*(\w|\d)*;*/g);
+        if(modExp !== null){
+            modSrc = modSrc.replace(modExp[0],"");
+            expName = modExp[0].replace(/(module.exports\s*=\s*|;)/g, "");
+        }
+
+        var mod = `;(function(){${modSrc} a7_i.${imp.name}=${expName};})();var ${imp.name}=a7_i.${imp.name};`;
+
+        if(config.mode === "production"){
+            mod = minifier(mod);
+        }
+        //replacing the import on the sourcecode with the modules contens
+        sourceCode = sourceCode.replace(Import, "/* " + imp.name + " */" + mod);
+        imports += {
+            from:imp.path,
+            as:imp.name
+        };
+    };
+
+    len = 0;
     if (partialImports !== null){
         this.imports += partialImports.length;
-        partialImports.forEach(Import =>{
-            var imp = importHandler(Import);
-        });
+        len = partialImports.length;
         return core.errorLog("Importing only a part of a framework or a library is not yet supported!");
     }
+    for(let i = 0; i < len; i++){
+        var Import = partialImports[i];
+        var imp = importHandler(Import);
+    };
 
     if (CSSBundle != ""){
         sourceCode += "a7.loadCSS(\""+cssMinifier(CSSBundle)+"\")";
     }
 
     if (config.mode === "production"){
-        sourceCode = uglifyJS.minify("(function(){" + sourceCode + "})()", {
-            compress:{passes:1},
-            mangle:{toplevel:true}
+        sourceCode = uglifyJS.minify(`(function(){${sourceCode}})()`, {
+            compress:{passes:1}, mangle:{toplevel:true}
         }).code;
     }
     core.successLog("app was built.");
