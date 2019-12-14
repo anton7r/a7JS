167445903c42570f13623bc44b53cf2e8636e78c
anton7r <52160345+anton7r@users.noreply.github.com>
2019-12-14 21:11:34 +0200

    delete test folder and add more test

---
+++ cli/core/fsx.test.js
@@ -0,0 +1,9 @@
+const fsx = require("./fsx");
+
+test('should return path with forward slashes', () => {
+    expect(fsx.formatSlashes("./dir\\2/3\\4")).toBe("./dir/2/3/4")
+});
+
+test('should return path without ../ in the midle', () => {
+    expect(fsx.purePath("./dir/../")).toBe("./");
+});
