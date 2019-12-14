4510d860e13e66c6f18ba84d667e250d25c9cf1a
anton7r <52160345+anton7r@users.noreply.github.com>
2019-12-14 21:10:53 +0200

    compare-versions done

--- cli/core/compare-versions.js
+++ cli/core/compare-versions.js
@@ -3,17 +3,100 @@
 //returns true when the newV is newer than oldV
 module.exports = {
     isNewer(newV, oldV){
-        oldVersion = oldV.split(".");
-        newVersion = newV.split(".");
+        var oldVersion = oldV.split(".");
+        var newVersion = newV.split(".");
         
-        for(var i = 0; i < newVersion.length; i++){
-            
-            //currently compares only numbers
+        var i = 0;
+        while(true){
+            var newHasDash = false;
+            var oldHasDash = false;
+
+            if(newVersion[i].includes("-")){
+                newVersion[i] = newVersion[i].split("-")[0];
+                newHasDash = true;
+            }
+
+            if(oldVersion[i].includes("-")){
+                oldVersion[i] = oldVersion[i].split("-")[0];
+                oldHasDash = true;
+            }
+
             if(newVersion[i] > oldVersion[i]){
+                return true;   
+            } else if (oldHasDash === true && newHasDash === false && newVersion[i] == oldVersion[i]){
                 return true;
-            } else if (newVersion.length - 1 === i){
+            } else if (oldHasDash === false && newHasDash === true && newVersion[i] == oldVersion[i]){
+                return false;
+            } else if (oldHasDash === true && newHasDash === true){
+                break;
+            } else if (newVersion.length - 1 === i && oldVersion.length - 1 === i){
+                return false;
+            } else if (newVersion.length - 1 === i && oldVersion.length - 1 !== i){
+                break;
+            } else if (newVersion.length - 1 !== i && oldVersion.length - 1 === i){
+                break;
+            }
+            i++;
+        };
+
+        var newVstage = newV.split("-")[1];
+        var oldVstage = oldV.split("-")[1];
+        var newStage = 0;
+        var oldStage = 0;
+
+        //convers alpha to 0 and  beta to 1 and rc to 2
+        // then compares them
+        if(newVstage.indexOf("alpha") === 0){
+            newVstage = newVstage.replace("alpha", "");
+        } else if(newVstage.indexOf("beta") === 0){
+            newStage = 1;
+            newVstage = newVstage.replace("beta", "");
+        } else if(newVstage.indexOf("rc") === 0){
+            newStage = 2;
+            newVstage = newVstage.replace("rc", "")
+        } else {
+            return console.error("Unsupported stage");
+        }
+
+        if(oldVstage.indexOf("alpha") === 0){
+            oldVstage = oldVstage.replace("alpha", "");
+        } else if (oldVstage.indexOf("beta") === 0){
+            oldStage = 1;
+            oldVstage = oldVstage.replace("beta", "");
+        } else if(oldVstage.indexOf("rc") === 0){
+            oldStage = 2;
+            oldVstage = oldVstage.replace("rc", "");
+        } else {
+            return console.error("Unssupported stage");
+        }
+    
+        if (newStage > oldStage){
+            return true;
+        } else if (oldStage > newStage){
+            return false;
+        }
+
+        var newerStages = newVstage.split(".");
+        var olderStages = oldVstage.split(".");
+    
+        if(newerStages[0] === ""){
+            newerStages[0] = 1;
+        }
+        
+        if (olderStages[0] === ""){
+            olderStages[0] = 1;
+        }
+
+        i = 0;
+        while(true){
+            if(newerStages[i] > olderStages[i]){
+                return true;
+            } else if (newerStages.length - 1 === i){
                 return false;
             }
-        };
+
+            i++;
+        }
+        console.log(olderStages, newerStages);
     }
 }
---
+++ cli/core/compare-versions.test.js
@@ -0,0 +1,25 @@
+const version = require("./compare-versions");
+
+test("should output true because the version is newer", () => {
+    expect(version.isNewer("1.2.2", "1.2.1")).toBe(true);
+})
+
+test("should output false because the \"newer\" version is older", () => {
+    expect(version.isNewer("1.6.5", "1.6.6")).toBe(false);
+})
+
+test('should output true because the -rc is stripped from the version number', () => {
+    expect(version.isNewer("1.0.1", "1.0.0-rc1")).toBe(true);
+});
+
+test("should output true because the older version is a release candidate", () => {
+    expect(version.isNewer("1.0.0", "1.0.0-rc1.3")).toBe(true);
+})
+
+test('both are beta', () => {
+    expect(version.isNewer("1.0.0-beta2", "1.0.0-beta")).toBe(true);
+});
+
+test('beta vs alpha', () => {
+    expect(version.isNewer("1.0.0-beta", "1.0.0-alpha")).toBe(true);
+});
