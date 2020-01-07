7ae33681cd0a83c0418b6cfec95bc1947a61ad51
Särkioja Lauri Amos Ilmari <lauri.sarkioja@edu.tampere.fi>
2020-01-07 13:30:35 +0200

    fixed a typo

--- cli/core/fsx.test.js
+++ cli/core/fsx.test.js
@@ -4,6 +4,6 @@ test('should return path with forward slashes', () => {
     expect(fsx.formatSlashes("./dir\\2/3\\4")).toBe("./dir/2/3/4")
 });
 
-test('should return path without ../ in the midle', () => {
+test('should return path without ../ in the middle', () => {
     expect(fsx.purePath("./dir/../")).toBe("./");
 });
