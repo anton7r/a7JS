974e61a20a94c44139d9ca4618f2097a01ccd670
Lielahti Tapio Johannes <tapio.lielahti@edu.tampere.fi>
2020-01-09 10:55:20 +0200

    errormsg update

--- cli/dev-server/errormsg.js
+++ cli/dev-server/errormsg.js
@@ -1,6 +1,30 @@
 //this file will be executed on the frontend!
 
+var css = `body{
+    background-color:rgb(27, 27, 27);
+}
+header{
+    font-family: Arial;
+    font-size: 30px;
+    color: red;
+    margin-top: 20px;
+    margin-left: 150px;
+}
+#file{
+    font-family: arial;
+    font-size: 22px;
+    text-align: left;
+    margin-left: 150px;
+    color: white;
+}
+`
+
+var template = `<header>Error: $errormsg</header>
+<div id="file">
+    <p> $file<br>$file2</p>
+</div>`
 
 addEventListener("error", function(ev){
-    ev.error;
+    
+    
 });
