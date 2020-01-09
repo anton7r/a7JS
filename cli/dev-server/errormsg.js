d26272bb606f65e465a1b0332a59b654fba413a1
unknown <anton.rosnell@elisanet.fi>
2020-01-09 11:27:22 +0200

    beautifying code

--- cli/dev-server/errormsg.js
+++ cli/dev-server/errormsg.js
@@ -1,31 +1,34 @@
 //this file will be executed on the frontend!
 
-var css = `body{
+var css = `
+body {
     background-color:rgb(27, 27, 27);
 }
-header{
+
+header {
     font-family: Arial;
     font-size: 30px;
     color: red;
     margin-top: 20px;
     margin-left: 150px;
 }
-#file{
+
+#file {
     font-family: arial;
     font-size: 22px;
     text-align: left;
     margin-left: 150px;
     color: white;
-}
-`
+}`;
 
-var template = `<header>Error: errormsg</header>
+var template = `
+<header>Error: errormsg</header>
 <div id="file">
     <p> file1</p>
-</div>`
+</div>`;
 
 addEventListener("error", function(ev){
-    var message = ev.message
-    var file = ev.filename
-    var t = template.replace("errormsg", message).replace("file1", file)
+    var message = ev.message;
+    var file = ev.filename;
+    var t = template.replace("errormsg", message).replace("file1", file);
 });
