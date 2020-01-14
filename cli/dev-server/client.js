66a01fdcf32cbe7b450e292c35e203d1391e12cc
Särkioja Lauri Amos Ilmari <lauri.sarkioja@edu.tampere.fi>
2020-01-14 08:40:14 +0200

    websocket update

--- cli/dev-server/client.js
+++ cli/dev-server/client.js
@@ -40,11 +40,11 @@ var socket = new WebSocket("localhost:{{ port }}");
 
 socket.onopen = function() {
     alert("[Auto Refresh] Connection established");
-    socket.send("Io sono Aldo");
 };
 
 socket.onmessage = function(ev) {
-    alert(`[message] Data received from server: ${ev.data}`);
+    var msg = ev.data;
+    
 };
 
 socket.onclose = function(ev) {
