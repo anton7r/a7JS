209ad9373d97c6d0987ff5f9f8a4b65aed1c881d
unknown <anton.rosnell@elisanet.fi>
2020-02-20 16:54:29 +0200

    beautified

--- cli/compiler/css-minifier.js
+++ cli/compiler/css-minifier.js
@@ -1,9 +1,8 @@
 var csso = require("csso");
-module.exports = function(css){
+module.exports = css => {
     try {
         return csso.minify(css).css;
-    }
-    catch(e){
+    } catch (e) {
         return css.replace(/\n\r/g, "");
     }
 };
