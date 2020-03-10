c33d0c29fa0d1ad6cdc211a1682ed282a9258332
Lo L <tapio.lielahti@gmail.com>
2020-03-10 09:38:51 +0200

    update compiler working now

--- src/a7.js
+++ src/a7.js
@@ -145,7 +145,6 @@ a7.secureProps = mode => {
 
 //REVIEW:
 a7.createElement = function (element, attributes) {
-    console.log(element);
     //Replace this
     var props;
     var component = a7store[1][element];
